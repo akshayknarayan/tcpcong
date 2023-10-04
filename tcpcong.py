4dff458de03a4c1dee0f3631a78c268b4628271b
Akshay Narayan <akshayn@icsi.berkeley.edu>
2023-10-04 14:38:35 -0700

    cwnd+rtt graphs

--- tcpcong.py
+++ tcpcong.py
@@ -29,6 +29,51 @@ def load(flows):
     #bpf.attach_tracepoint(tp="tcp:tcp_probe", fn_name="tracepoint__tcp__tcp_probe")
     return bpf
 
+class FlowGraph:
+    def __init__(self, flowid):
+        self.flowid = flowid
+        self.cwnds = []
+        self.rtts = []
+
+    def observation(self, cwnd: int, rtt: float):
+        now = time.time()
+        self.cwnds.append((now, cwnd))
+        if len(self.cwnds) > 10:
+            while self.cwnds[-1][0] - self.cwnds[0][0] > 10:
+                self.cwnds = self.cwnds[1:]
+        self.rtts.append((now, rtt))
+        if len(self.rtts) > 10:
+            while self.rtts[-1][0] - self.rtts[0][0] > 10:
+                self.rtts = self.rtts[1:]
+
+    def cwnd_graph(self, fn):
+        now = time.time()
+        times, cwnds = zip(*self.cwnds)
+        times = [t - now for t in times]
+        return fn(xs=times, ys=cwnds, lines=True, title=self.flowid + " cwnd")
+
+    def print_cwnd_graph(self):
+        from uniplot import plot
+        self.cwnd_graph(plot)
+
+    def get_cwnd_graph(self) -> [str]:
+        from uniplot import plot_to_string
+        return self.cwnd_graph(plot_to_string)
+
+    def rtt_graph(self, fn):
+        now = time.time()
+        times, rtts = zip(*self.rtts)
+        times = [t - now for t in times]
+        return fn(xs=times, ys=rtts, lines=True, title=self.flowid + " RTT")
+
+    def print_rtt_graph(self):
+        from uniplot import plot
+        return self.rtt_graph(plot)
+
+    def get_rtt_graph(self) -> [str]:
+        from uniplot import plot_to_string
+        return self.rtt_graph(plot_to_string)
+
 def get_addr(addr_bytes, family):
     if family == socket.AF_INET:
         return ipaddress.IPv4Address(addr_bytes[:4])
@@ -37,14 +82,18 @@ def get_addr(addr_bytes, family):
     else:
         raise Exception("unsupported address family")
 
-def poll(bpf):
+def poll(bpf, flow_hist):
     from json import JSONDecodeError
     try:
         flows = bpf["flows"]
         for k, v in flows.items():
             saddr = get_addr(bytes(k.saddr), k.family)
             daddr = get_addr(bytes(k.daddr), k.family)
-            print(f"[{saddr}:{k.sport} -> {daddr}:{k.dport}] cwnd {v.snd_cwnd} srtt {v.srtt}")
+            #print(f"[{saddr}:{k.sport} -> {daddr}:{k.dport}] cwnd {v.snd_cwnd} srtt {v.srtt}")
+            flowid = f"{saddr}:{k.sport} -> {daddr}:{k.dport}"
+            if flowid not in flow_hist:
+                flow_hist[flowid] = FlowGraph(flowid)
+            flow_hist[flowid].observation(v.snd_cwnd, v.srtt)
     except JSONDecodeError as e:
         print(e)
         print('failed', e.doc)
@@ -64,6 +113,13 @@ if __name__ == '__main__':
         rules.append((int(src) if src != '*' else None, int(dst) if dst != '*' else None))
     bpf = load(rules)
 
+    flows = {}
+    last_print = time.time()
     while True:
-        time.sleep(1)
-        poll(bpf)
+        time.sleep(0.1)
+        poll(bpf, flows)
+        if time.time() - last_print > 5:
+            for f in flows:
+                flows[f].print_cwnd_graph()
+                flows[f].print_rtt_graph()
+            last_print = time.time()
